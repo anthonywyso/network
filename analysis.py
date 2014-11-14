f436f4d4619b536735d1edf3932fa8ed84bad49b
anthonywyso <anthonywyso@gmail.com>
2014-11-13 18:32:09 -0800

    updated for player-player relationships, using direct sqlite queried data

--- analysis.py
+++ analysis.py
@@ -2,6 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import pandas as pd
 import re
+import sqlite3 as sql
 
 
 class NBANetwork(object):
@@ -19,7 +20,6 @@ class NBANetwork(object):
         self.G = nx.DiGraph(season=self.season)
         for (player, coach, weight) in data.values:
             self.G.add_edge(player, coach, weight=weight)
-        # self.G = nx.read_edgelist(fname, delimiter=",", nodetype=str, edgetype=float, data=(('weight', float),))
 
     def load_nodes(self, fname="data/players_rapm_id.csv"):
         data = pd.read_csv(fname)
@@ -28,11 +28,11 @@ class NBANetwork(object):
             try:
                 self.G.node[node]['rapm_both'] = data[data['id'] == node]['rapm_both'].values[0]
             except:
-                self.G.node[node]['rapm_both'] = data.median()
+                self.G.node[node]['rapm_both'] = data.median()['rapm_both']
 
     def label_nodes(self):
-        self.coach_nodes = [node for node in self.G.nodes() if re.search('c\Z' , node)]
-        self.player_nodes = [node for node in self.G.nodes() if not re.search('c\Z' , node)]
+        self.coach_nodes = [node for node in self.G.nodes() if re.search('c\Z', node)]
+        self.player_nodes = [node for node in self.G.nodes() if not re.search('c\Z', node)]
 
     def weigh_edges(self):
         for player, coach, val in self.G.edges(data=True):
@@ -40,8 +40,7 @@ class NBANetwork(object):
 
     def viz_network(self):
         edge_width = [d['weight']*2 for (u, v, d) in self.G.edges(data=True)]
-
-        node_pos = nx.spring_layout(self.G, k=.01, scale=1)
+        node_pos = nx.spring_layout(self.G, k=.001, scale=5)
 
         f = plt.figure(figsize=(32, 20))
         ax = f.add_subplot(1, 1, 1)
@@ -57,6 +56,42 @@ class NBANetwork(object):
         self.load_nodes()
         self.label_nodes()
         self.weigh_edges()
+
+
+class NBAPlayerNetwork(object):
+
+    def __init__(self, season=2014):
+        self.season = season
+        self.G = None
+        self.coach_nodes = None
+        self.player_nodes = None
+
+    def load_edges(self, dbname='data/network.sqlite'):
+        query = '''
+                SELECT *
+                FROM player_player_tenures
+                WHERE season >= 2000
+                '''
+        with sql.connect(dbname) as con:
+            data = pd.read_sql(query, con)
+        data.pop('season')
+        data.pop('team_id')
+        self.G = nx.DiGraph(season=self.season)
+        for (player, coach, weight) in data.values:
+            self.G.add_edge(player, coach, weight=weight)
+
+    def viz_network(self):
+        edge_width = [d['weight']*2 for (u, v, d) in self.G.edges(data=True)]
+        node_pos = nx.spring_layout(self.G, k=.001, scale=5)
+
+        f = plt.figure(figsize=(32, 20))
+        ax = f.add_subplot(1, 1, 1)
+        ax.set_title('NBA Player-Player Relationships (as of %s)' % self.season)
+        nx.draw_networkx_nodes(self.G, pos=node_pos, node_color='black', alpha=1.0)
+        # nx.draw_networkx_nodes(self.G, pos=node_pos, nodelist=self.coach_nodes, node_color='black', alpha=1.0, label="coaches")
+        # nx.draw_networkx_nodes(self.G, pos=node_pos, nodelist=self.player_nodes, alpha=0.1, label="players")
+        nx.draw_networkx_edges(self.G, pos=node_pos, width=edge_width, edge_color=edge_width)
+        plt.show()
 
 
 def main():
