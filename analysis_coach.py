62d154fd5cbccc9c34b4147f6e089c3ad9a8e5fa
anthonywyso <anthonywyso@gmail.com>
2014-11-14 16:44:47 -0800

    updated for coach RAPM analysis

--- analysis_coach.py
+++ analysis_coach.py
@@ -49,7 +49,7 @@ class CoachValue(object):
         plt.show()
 
 
-class CoachRookies(object):
+class CoachFirstYear(object):
 
     def __init__(self, dbname='data/network.sqlite'):
         self.default_size = (16, 10)
@@ -76,14 +76,14 @@ class CoachRookies(object):
         data.columns = ['player_id','name','rapm_off','rapm_def','rapm_both','season','poss','coach_id','player_coach_tenure_pct', 'rapm_off_impact', 'rapm_def_impact', 'rapm_both_impact']
         return data
 
-    def viz_performance_hist(self, data):
+    def display_hist(self, data, metric="rapm_both"):
         fig = plt.figure(figsize=self.default_size)
         ax = fig.add_subplot(1, 1, 1)
-        data['rapm_both'].hist(ax=ax, label="population", normed=True)
+        data[metric].hist(ax=ax, label="population", normed=True)
         plt.legend()
         plt.show()
 
-    def viz_performance_all(self, data):
+    def display_hist_all(self, data, data_population, metric="rapm_both"):
         eligibility = data.groupby(['coach_id']).count()['player_id'].median()
         eligible = data.groupby(['coach_id']).count()['player_id'] > eligibility
         eligible = eligible.reset_index()
@@ -93,40 +93,70 @@ class CoachRookies(object):
         for i, coach in enumerate(coaches_eligible['coach_id']):
             ax = fig.add_subplot(9, 9, i+1)
             ax.set_title(coach)
-            data['rapm_both'].hist(ax=ax, label="population", alpha=.50, normed=True, grid=False, ylabelsize=0)
-            data[data['coach_id'] == coach]['rapm_both'].hist(ax=ax, label=coach, alpha=.40, normed=True, grid=False, ylabelsize=0)
+            data_population[metric].hist(ax=ax, label="population", alpha=.50, normed=True, grid=False, ylabelsize=0)
+            data[data['coach_id'] == coach][metric].hist(ax=ax, label=coach, alpha=.40, normed=True, grid=False, ylabelsize=0)
         # plt.legend()
         plt.tight_layout()
         plt.show()
 
-    def ks(self, data):
+    def ks(self, data, data_population, metric="rapm_both"):
         coaches_ks = defaultdict(list)
         for coach in data['coach_id']:
-            d, p = ks_2samp(data[data['coach_id'] == coach]['rapm_both'], data['rapm_both'])
+            d, p = ks_2samp(data[data['coach_id'] == coach][metric], data_population[metric])
             coaches_ks[coach] = [d, p]
 
         coaches_score = pd.DataFrame.from_dict(coaches_ks, orient='index').reset_index()
         coaches_score.columns = ['coach_id', 'd', 'p']
 
-        rookies_coached = data.groupby(['coach_id']).count()['player_id'].reset_index()
-        crit_vals = 1.36 * np.sqrt((rookies_coached['player_id'] + data.shape[0]) / (rookies_coached['player_id'] * data.shape[0]))
-        coaches_crit = pd.concat([rookies_coached, pd.DataFrame(crit_vals)], axis=1)
-        coaches_crit.columns = ['coach_id', 'rookie_count', 'crit']
+        players_coached = data.groupby(['coach_id']).count()['player_id'].reset_index()
+        crit_vals = 1.36 * np.sqrt((players_coached['player_id'] + data.shape[0]) / (players_coached['player_id'] * data.shape[0]))
+        coaches_crit = pd.concat([players_coached, pd.DataFrame(crit_vals)], axis=1)
+        coaches_crit.columns = ['coach_id', 'player_count', 'crit']
 
         ks_grid = coaches_crit.merge(coaches_score, on=['coach_id'])
         return ks_grid
 
-    def t(self, data):
+    def t(self, data, data_population, metric="rapm_both"):
         coaches_t = defaultdict(list)
         for coach in data['coach_id']:
-            _, p = ttest_ind(data[data['coach_id'] == coach]['rapm_both'], data['rapm_both'])
-            coaches_t[coach] = p
+            t, p = ttest_ind(data[data['coach_id'] == coach][metric], data_population[metric])
+            coaches_t[coach] = [t, p]
 
         coaches_score = pd.DataFrame.from_dict(coaches_t, orient='index').reset_index()
-        coaches_score.columns = ['coach_id', 'p']
-        return coaches_score
+        coaches_score.columns = ['coach_id', 't', 'p']
 
-class CoachSophs(CoachRookies):
+        players_coached = data.groupby(['coach_id']).count()['player_id'].reset_index()
+        players_coached.columns = ['coach_id', 'player_count']
 
-    def load_data(self):
-        pass
+        t_grid = players_coached.merge(coaches_score, on=['coach_id'])
+        return t_grid
+
+    def display_coach(self, data, coach, metric="rapm_both", cols=[]):
+        """
+        INPUT: DataFrame, str, str, list
+        OUTPUT: DataFrame
+        Displays players and their metrics under a particular coach
+        """
+        return data[data['coach_id'] == coach].sort([metric])[cols]
+
+
+class CoachMultiYear(CoachFirstYear):
+
+    def load_data(self, dbname2="players_soph_performances", y_delta=1):
+        """
+        INPUT: str, int
+        OUTPUT: DataFrame
+        Loads data from specified db and year delta; returns dataframe of player performances
+        """
+        # TODO protect against db injection attacks
+        query = " ".join(["SELECT y1.*, y2.rapm_both AS rapm_both_y2, y2.rapm_both - y1.rapm_both AS rapm_both_delta FROM players_rookie_performances AS y1 JOIN", dbname2, "AS y2 ON y1.id = y2.id AND y1.season = y2.season -", str(y_delta), "AND y1.coach_id = y2.coach_id"])
+        query_population = " ".join(["SELECT y1.*, y2.rapm_both AS rapm_both_y2, y2.rapm_both - y1.rapm_both AS rapm_both_delta FROM players_rookie_performances AS y1 JOIN", dbname2, "AS y2 ON y1.id = y2.id AND y1.season = y2.season -", str(y_delta)])
+        with sql.connect(self.db) as con:
+            cur = con.cursor()
+            cur.execute(query)
+            data = pd.DataFrame(cur.fetchall())
+            cur.execute(query_population)
+            data_population = pd.DataFrame(cur.fetchall())
+        data.columns = ['player_id','name','rapm_off','rapm_def','rapm_both','season','poss','coach_id','player_coach_tenure_pct', 'rapm_off_impact', 'rapm_def_impact', 'rapm_both_impact', 'rapm_both_y2', 'rapm_both_delta']
+        data_population.columns = ['player_id','name','rapm_off','rapm_def','rapm_both','season','poss','coach_id','player_coach_tenure_pct', 'rapm_off_impact', 'rapm_def_impact', 'rapm_both_impact', 'rapm_both_y2', 'rapm_both_delta']
+        return data, data_population
